36f329360d273d41d7fdfb838fd146f3d2055312
atique-atq <atique.atq@gmail.com>
2022-09-29 19:22:16 +0600

    added clicking

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Cart.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationPin } from '@fortawesome/free-solid-svg-icons';
 import profilepic from '../../images/profilepic.jpg';
 
 const Cart = ({ totalTime }) => {
+
+    const [breakValue, setBreakValue] = useState(0);
+
+    const handleBreakToCart = (vacation) => {
+        setBreakValue(vacation);
+    }
  
     return (
         <div className='cart'>
@@ -30,19 +36,19 @@ const Cart = ({ totalTime }) => {
             <div className='break-time'>
                 <h4> Add Reserve Days/Vacation</h4>
                 <div className='break-options'>
-                    <div className='single-break'>
+                    <div className='single-break' onClick={() => handleBreakToCart(5)}>
                         <div>05</div>
                         <div>days</div>
                     </div>
-                    <div className='single-break'>
+                    <div className='single-break' onClick={() => handleBreakToCart(10)}>
                         <div>10</div>
                         <div>days</div>
                     </div>
-                    <div className='single-break'>
+                    <div className='single-break' onClick={() => handleBreakToCart(15)}>
                         <div>15</div>
                         <div>days</div>
                     </div>
-                    <div className='single-break'>
+                    <div className='single-break'onClick={() => handleBreakToCart(20)}>
                         <div>20</div>
                         <div>days</div>
                     </div>
@@ -52,8 +58,8 @@ const Cart = ({ totalTime }) => {
             <div className='time-calculation'>
                 <h3>Required Investing Time</h3>
                 <div className='timefield-container'>
-                    <div className='timefield'>Course duration: <span>{totalTime} days</span> </div>
-                    <div className='timefield'>Reserve days  &nbsp; &nbsp; &nbsp;: <span></span> </div>
+                    <div className='timefield'>Course duration days: <span>{totalTime === 0? null : totalTime} </span> </div>
+                    <div className='timefield'>Reserve days  &nbsp;: <span>{breakValue === 0? null : breakValue} </span></div>
                 </div>
             </div>
 
